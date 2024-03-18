b7ad96d91e6461d761b9fd1ae431a90fa86d1c59
aziz-shoko <azizbek.shokosimov@gmail.com>
2024-03-18 12:18:07 -0500

    add debug statements to see whats going wrong

--- aws.js
+++ aws.js
@@ -9,13 +9,16 @@ async function aws(db, table_name, pr) {
 
   core.debug(`Checking for active environment assigned to ${pr}...`);
   const current_envs = await db.scan(scanByPr).promise(); 
+  core.debug(`Current environments: ${JSON.stringify(current_envs)}`);
 
   if (current_envs.Count === 1) { 
     core.debug(`Found an active QA environment for ${pr}`);
     const env = current_envs.Items[0];
+    core.debug(`Environment details: ${JSON.stringify(env)}`);
 
     core.debug(`Removing ${pr} from QA environment ...`);
     const updatedPullRequests = env.pull_requests.filter(item => item.S !== pr); 
+    core.debug(`Updated pull requests: ${JSON.stringify(updatedPullRequests)}`);
     const in_use = updatedPullRequests.length > 0; 
     const branch = in_use ? env.branch.S : ''; 
 
@@ -31,9 +34,14 @@ async function aws(db, table_name, pr) {
         ':pull_requests': { L: updatedPullRequests },
       },
     };
+    core.debug(`Update operation: ${JSON.stringify(update)}`);
 
-    await db.update(update).promise();
-    core.debug(`Updated QA environment: in_use=${in_use}, branch=${branch}, pull_requests=${JSON.stringify(updatedPullRequests)}`);
+    try {
+      await db.update(update).promise();
+      core.debug(`Updated QA environment: in_use=${in_use}, branch=${branch}, pull_requests=${JSON.stringify(updatedPullRequests)}`);
+    } catch (error) {
+      core.setFailed(`Error updating QA environment: ${error.message}`);
+    }
   } else if (current_envs.Count > 1) {
     throw new Error(`PR is assigned to multiple QA environments (${current_envs.Items.map(item => `${item.env_name.S}, `)})`);
   } else {
