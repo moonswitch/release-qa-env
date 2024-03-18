10a19b2efbade7c6814eaeca72f704b41898cba2
aziz-shoko <azizbek.shokosimov@gmail.com>
2024-03-18 12:01:04 -0500

    updated javascript code

--- aws.js
+++ aws.js
@@ -1,27 +1,24 @@
 const core = require('@actions/core');
 
-let aws = async function (db, table_name, pr) {
+async function aws(db, table_name, pr) {
   const scanByPr = {
     TableName: table_name,
     FilterExpression: 'contains (pull_requests, :pull_request)',
     ExpressionAttributeValues: { ':pull_request': pr },
-  }
+  };
 
   core.debug(`Checking for active environment assigned to ${pr}...`);
-  const current_envs = await db.scan(scanByPr);
+  const current_envs = await db.scan(scanByPr).promise(); 
 
-  if (current_envs.Count == 1) {
+  if (current_envs.Count === 1) { 
     core.debug(`Found an active QA environment for ${pr}`);
     const env = current_envs.Items[0];
-    let in_use = env.pull_requests.length > 0;
-    let branch = in_use ? env.branch : '';
-
-    core.debug(`Found QA environment in use by ${env.pull_requests}`);
 
     core.debug(`Removing ${pr} from QA environment ...`);
-    env.pull_requests.splice(env.pull_requests.indexOf(pr), 1);
-    in_use = env.pull_requests.length > 0;
-    branch = in_use ? env.branch : '';
+    const updatedPullRequests = env.pull_requests.filter(item => item.S !== pr); 
+    const in_use = updatedPullRequests.length > 0; 
+    const branch = in_use ? env.branch.S : ''; 
+
     const update = {
       TableName: table_name,
       Key: {
@@ -29,19 +26,19 @@ let aws = async function (db, table_name, pr) {
       },
       UpdateExpression: 'set in_use = :in_use, branch = :branch, pull_requests = :pull_requests',
       ExpressionAttributeValues: {
-        ':in_use': in_use,
-        ':branch': branch,
-        ':pull_requests': env.pull_requests,
+        ':in_use': { BOOL: in_use },
+        ':branch': { S: branch },
+        ':pull_requests': { L: updatedPullRequests },
       },
     };
-  
-    await db.update(update);
-    
+
+    await db.update(update).promise();
+    core.debug(`Updated QA environment: in_use=${in_use}, branch=${branch}, pull_requests=${JSON.stringify(updatedPullRequests)}`);
   } else if (current_envs.Count > 1) {
-    throw new Error(`PR is assigned to multiple QA environments (${current_envs.Items.map(item => `${item.env_name}, `)})`);
+    throw new Error(`PR is assigned to multiple QA environments (${current_envs.Items.map(item => `${item.env_name.S}, `)})`);
   } else {
     core.debug(`No active environment found for ${pr}. Nothing to do.`);
   }
-};
+}
 
 module.exports = aws;
