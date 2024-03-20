dbd284d1677c27e4c40f5fd60bf1b33a6fe106a2
aziz-shoko <azizbek.shokosimov@gmail.com>
2024-03-20 14:09:26 -0500

    change it back to my code solution

--- aws.js
+++ aws.js
@@ -1,26 +1,30 @@
 const core = require('@actions/core');
 
-async function aws(db, table_name, pr) {
+let aws = async function (db, table_name, pr) {
   const scanByPr = {
     TableName: table_name,
     FilterExpression: 'contains (pull_requests, :pull_request)',
     ExpressionAttributeValues: { ':pull_request': pr },
-  };
+  }
 
   core.debug(`Checking for active environment assigned to ${pr}...`);
-  const current_envs = await db.scan(scanByPr).promise(); 
-  core.debug(`Current environments: ${JSON.stringify(current_envs)}`);
+  const current_envs = await db.scan(scanByPr);
 
-  if (current_envs.Count == 1) { 
+  if (current_envs.Count == 1) {
     core.debug(`Found an active QA environment for ${pr}`);
     const env = current_envs.Items[0];
-    core.debug(`Environment details: ${JSON.stringify(env)}`);
+
+    let in_use = env.pull_requests.length > 0;
+    let branch = in_use ? env.branch : '';
+
+    core.debug(`Found QA environment in use by ${env.pull_requests}`);
 
     core.debug(`Removing ${pr} from QA environment ...`);
-    const updatedPullRequests = env.pull_requests.filter(item => item.S !== pr); 
-    core.debug(`Updated pull requests: ${JSON.stringify(updatedPullRequests)}`);
-    const in_use = updatedPullRequests.length > 0; 
-    const branch = in_use ? env.branch.S : ''; 
+    env.pull_requests.splice(env.pull_requests.indexOf(pr), 1);
+
+    core.debug(`Updating DynamoDB...`);
+    in_use = env.pull_requests.length > 0;
+    branch = in_use ? env.branch : '';
 
     const update = {
       TableName: table_name,
@@ -29,24 +33,19 @@ async function aws(db, table_name, pr) {
       },
       UpdateExpression: 'set in_use = :in_use, branch = :branch, pull_requests = :pull_requests',
       ExpressionAttributeValues: {
-        ':in_use': { BOOL: in_use },
-        ':branch': { S: branch },
-        ':pull_requests': { L: updatedPullRequests },
+        ':in_use': in_use,
+        ':branch': branch,
+        ':pull_requests': env.pull_requests,
       },
     };
-    core.debug(`Update operation: ${JSON.stringify(update)}`);
-
-    try {
-      await db.update(update).promise();
-      core.debug(`Updated QA environment: in_use=${in_use}, branch=${branch}, pull_requests=${JSON.stringify(updatedPullRequests)}`);
-    } catch (error) {
-      core.setFailed(`Error updating QA environment: ${error.message}`);
-    }
+  
+    await db.update(update);
+    
   } else if (current_envs.Count > 1) {
-    throw new Error(`PR is assigned to multiple QA environments (${current_envs.Items.map(item => `${item.env_name.S}, `)})`);
+    throw new Error(`PR is assigned to multiple QA environments (${current_envs.Items.map(item => `${item.env_name}, `)})`);
   } else {
     core.debug(`No active environment found for ${pr}. Nothing to do.`);
   }
-}
+};
 
 module.exports = aws;
